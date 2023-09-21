86d9ebccdd379afbfcd70fc6577df1810ea0f689
ThanhVu <huynhthanhvu115@gmail.com>
2023-09-21 18:09:07 +0700

    Update App.js

--- FeICC/App.js
+++ FeICC/App.js
@@ -2,7 +2,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Signup from './components/Signup';
 import Onbroading from './components/Onbroading';
 import Account from './navigation/Account';
 import CreateAccPage from './components/CreateAccPage';
